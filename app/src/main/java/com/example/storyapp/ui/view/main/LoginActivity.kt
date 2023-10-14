35f02012e10d55911327a8c1c4a8002537deecff
pierreviences <imaniyazid@gmail.com>
2023-10-14 17:18:54 +0700

    chore: Structure Folder

--- app/src/main/java/com/example/storyapp/ui/view/main/LoginActivity.kt
+++ app/src/main/java/com/example/storyapp/ui/view/main/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.storyapp
+package com.example.storyapp.ui.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.storyapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
