336f400759c020c109dfd8a4e61f89407a17e1c3
pierreviences <imaniyazid@gmail.com>
2023-10-15 15:07:08 +0700

    feat: Add Animation In Login Activity

--- app/src/main/java/com/example/storyapp/ui/view/main/LoginActivity.kt
+++ app/src/main/java/com/example/storyapp/ui/view/main/LoginActivity.kt
@@ -1,12 +1,49 @@
 package com.example.storyapp.ui.view.main
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.storyapp.R
+import com.example.storyapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        playAnimation()
+    }
+    private fun playAnimation() {
+        val fadeInDuration = 500L
+
+        val titleAnimator = createFadeInAnimator(binding.titleTextView, fadeInDuration)
+        val messageAnimator = createFadeInAnimator(binding.messageTextView, fadeInDuration)
+        val emailTextAnimator = createFadeInAnimator(binding.emailTextView, fadeInDuration)
+        val emailEditTextAnimator = createFadeInAnimator(binding.emailEditTextLayout, fadeInDuration)
+        val passwordTextAnimator = createFadeInAnimator(binding.passwordTextView, fadeInDuration)
+        val passwordEditTextAnimator = createFadeInAnimator(binding.passwordEditTextLayout, fadeInDuration)
+        val loginButtonAnimator = createFadeInAnimator(binding.btnLogin, fadeInDuration)
+        val registerTextAnimator = createFadeInAnimator(binding.textViewToRegister, fadeInDuration)
+
+        AnimatorSet().apply {
+            playSequentially(
+                titleAnimator,
+                messageAnimator,
+                emailTextAnimator,
+                emailEditTextAnimator,
+                passwordTextAnimator,
+                passwordEditTextAnimator,
+                loginButtonAnimator,
+                registerTextAnimator
+            )
+            startDelay = fadeInDuration
+        }.start()
+    }
+
+    private fun createFadeInAnimator(view: View, duration: Long): ObjectAnimator {
+        return ObjectAnimator.ofFloat(view, View.ALPHA, 1f).setDuration(duration)
     }
 }
