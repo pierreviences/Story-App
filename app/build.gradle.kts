84d0619c02f1299111f7d0af461ba8729d567133
pierreviences <imaniyazid@gmail.com>
2023-10-16 17:17:09 +0700

    feat: Add UserPreferences class for local user data management using DataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
---
+++ app/src/main/java/com/example/storyapp/data/local/UserPreferences.kt
@@ -0,0 +1,61 @@
+package com.example.storyapp.data.local
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.storyapp.data.model.LoginResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token_user")
+class UserPreferences private constructor(private val dataStore: DataStore<Preferences>) {
+    private val userIdKey = stringPreferencesKey(USER_ID_KEY)
+    private val userNameKey = stringPreferencesKey(USER_NAME_KEY)
+    private val userTokenKey = stringPreferencesKey(USER_TOKEN_KEY)
+
+    fun getUserLogin(): Flow<LoginResult> {
+        return dataStore.data.map {
+            LoginResult(
+                userId = it[userIdKey] ?: "null",
+                name = it[userNameKey] ?: "null",
+                token = it[userTokenKey] ?: ""
+            )
+        }
+    }
+    suspend fun saveUserLogin(data: LoginResult) {
+        dataStore.edit { preferences ->
+            preferences[userIdKey] = data.userId
+            preferences[userNameKey] = data.name
+            preferences[userTokenKey] = data.token
+        }
+    }
+
+    suspend fun deleteUserLogin() {
+        dataStore.edit {
+            it.remove(userNameKey)
+            it.remove(userIdKey)
+            it.remove(userTokenKey)
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserPreferences? = null
+
+        fun getInstance(dataStore: DataStore<Preferences>): UserPreferences {
+            return INSTANCE ?: synchronized(this) {
+                val instance = UserPreferences(dataStore)
+                INSTANCE = instance
+                instance
+            }
+        }
+
+
+        private const val USER_ID_KEY = "userId"
+        private const val USER_NAME_KEY = "name"
+        private const val USER_TOKEN_KEY = "token"
+    }
+}
